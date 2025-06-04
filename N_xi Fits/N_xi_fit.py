41c71f95eb9536aceed18892791e8d0f8e4a33ea
seththepeacock <seththepeacock@gmail.com>
2025-06-03 21:42:06 -0400

    about to start re-peak picking when necessary

---
+++ N_xi Fits/N_xi_fit.py
@@ -0,0 +1,309 @@
+import numpy as np
+from N_xi_fit_funcs import *
+import pickle
+import os
+import phaseco
+from phaseco import *
+import matplotlib.pyplot as plt
+import matplotlib.patheffects as pe
+from tqdm import tqdm
+from collections import defaultdict
+import pandas as pd
+
+# Initialize list for row dicts for xlsx file
+rows = []
+
+for rho in [0.7]:
+    for wf_idx in [0, 1, 2, 3]:
+        for species in ['Anole', 'Owl', 'Human']:
+            for dense_stft, const_N_pd in [(1, 1)]:
+                if species == 'Human' and wf_idx != 3:
+                    continue
+                print(f"Processing {species} {wf_idx}")
+                
+                "Get waveform"
+                wf, wf_fn, fs, good_peak_freqs, bad_peak_freqs = get_wf(species=species, wf_idx=wf_idx)
+                
+                # Apply a high pass filter
+                hpf_cutoff_freq = 300
+                wf = spectral_filter(wf, fs, hpf_cutoff_freq, type='hp')
+                
+                # Crop to desired length
+                wf_length = 30 if dense_stft else 60                  
+                wf = wf[:int(wf_length*fs)]
+                
+                "PARAMETERS"
+                # Coherence Parameters
+                # rho = None
+                snapping_rhortle = 0
+                tau = 2**13 / 44100 # Everyone uses the same tau
+                tauS = int(tau*fs)
+                min_xi = 0.001
+                delta_xi = min_xi
+                # skip_min_xi = True if (dense_stft, const_N_pd) == (0, 0) else False
+                skip_min_xi = False
+                force_recalc_coherences = 0
+                
+                
+                # Plotting options
+                plotting_colossogram = 0
+                plotting_peak_picks = 0
+                plotting_fits = 1
+                show_plots = 0
+                
+                # Z-Test Parameters
+                sample_hw = 1
+                alpha_z = 1e-9 # Minimum p-value for z-test; we assume noise unless p < alpha_z (so higher alpha_z means more signal bins)
+
+                # Fitting Parameters
+                decay_start_max_xi = 0 
+                trim_step = 1
+                A_max = np.inf # 1 or np.inf
+                sigma_weighting_power = 0 # > 0 means less weight on lower coherence bins in fit
+                
+                # Plotting parameters
+                plot_noise_means_on_fits = 1
+                plot_noise_bin_on_fits = 1
+                s_signal=5
+                s_noise=5
+                s_decayed = 100
+                marker_signal='o'
+                marker_noise='o'
+                marker_decayed='*'
+                lw_fit = 1.5
+                alpha_fit = 1
+                pe_stroke_fit = [pe.Stroke(linewidth=2, foreground='black', alpha=1), pe.Normal()]  
+                edgecolor_signal=None
+                edgecolor_noise='yellow'
+                edgecolor_decayed='black'
+                crop=False
+                colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+                
+                # Species specific params
+                
+                # Maximum xi value
+                max_xis = {
+                    'Anole': 0.1,
+                    'Owl': 0.1,
+                    'Human': 1.5
+                }
+                
+                # Maximum frequency to plot (in khz)
+                max_khzs = {
+                    'Anole': 6,
+                    'Human': 10,
+                    'Owl': 12
+                }
+                
+                # This determines where to start the fit as the latest peak in the range defined by xi=[0, decay_start_max_xi] 
+                decay_start_max_xis = {
+                    'Anole' : 0.01,
+                    'Owl' : 0.01,
+                    'Human' : 0.2
+                }
+
+                decay_start_max_xi = decay_start_max_xis[species]
+                max_khz = max_khzs[species]
+                max_xi = max_xis[species]
+                global_max_xi = max(max_xis.values()) if const_N_pd else None
+
+                "Set filepaths"
+                fn_id = rf"{species} {wf_idx}, const_Npd={const_N_pd}, dense_stft={dense_stft}, rho={rho}, snapping_rhortle={snapping_rhortle}, tau={tau*1000:.0f}ms, max_xi={max_xi}, wf_length={wf_length}s, HPF={hpf_cutoff_freq}Hz, wf={wf_fn.split('.')[0]}"
+                # Calclulate Npd if we're going to hold it constant
+                pkl_fn = f'{fn_id} (Coherences)'
+                N_xi_folder = r'N_xi Fits/'
+                pkl_folder = N_xi_folder + r'Pickles/'
+                fig_folder = N_xi_folder + rf'Figures/'
+
+                "Calculate things"
+                # Raise warning if tauS is not a power of two AND the samplerate is indeed 44100
+                if np.log2(tauS) != int(np.log2(tauS)) and fs == 44100:
+                    raise ValueError("tauS is not a power of 2, but the samplerate is 44100!")
+                # Get coherences
+                os.makedirs(pkl_folder, exist_ok=True)
+                if os.path.exists(pkl_folder + pkl_fn + '.pkl') and not force_recalc_coherences:
+                    with open(pkl_folder + pkl_fn + '.pkl', 'rb') as file:
+                        coherences, f, xis, tau, rho, N_pd_min, N_pd_max, seg_spacing, snapping_rhortle, wf_fn, species = pickle.load(file)
+                else:
+                    print(f"Calculating coherences for {fn_id}")
+                    coherences_dict = get_colossogram_coherences(wf, fs, min_xi, max_xi, delta_xi, tauS=tauS, rho=rho, const_N_pd=const_N_pd, snapping_rhortle=snapping_rhortle, dense_stft=dense_stft, global_max_xi=global_max_xi, skip_min_xi=skip_min_xi, return_dict=True)
+                    coherences = coherences_dict['coherences']
+                    f = coherences_dict['f']
+                    xis = coherences_dict['xis']    
+                    N_pd_min = coherences_dict['N_pd_min']
+                    N_pd_max = coherences_dict['N_pd_max']
+                    seg_spacing = coherences_dict['seg_spacing']
+                    snapping_rhortle = coherences_dict['snapping_rhortle']
+                    
+                    with open(pkl_folder + pkl_fn + '.pkl', 'wb') as file:
+                        # pickle.dump((coherences, f, xis, tau, rho, wf_fn, species), file)
+                        pickle.dump((coherences, f, xis, tau, rho, N_pd_min, N_pd_max, seg_spacing, snapping_rhortle, wf_fn, species), file)
+                
+                # Get peak bin indices
+                good_peak_idxs = np.argmin(np.abs(f[:, None] - good_peak_freqs[None, :]), axis=0) 
+                bad_peak_idxs = np.argmin(np.abs(f[:, None] - bad_peak_freqs[None, :]), axis=0)
+                
+                # Temporary hack
+                if skip_min_xi and coherences.shape[1] == xis.shape[0] + 1:
+                    coherences = coherences[:, 1:]
+                
+                "Plots"
+                if const_N_pd:
+                    if N_pd_min != N_pd_max:
+                        raise Exception("If N_pd is constant, then N_pd_min and N_pd_max should be equal...")
+                    N_pd_str = f"$N_{{pd}}={N_pd_min}$"
+                else:
+                    N_pd_str = f"$N_{{pd}} \in [{N_pd_min}, {N_pd_max}]$"
+                if snapping_rhortle:
+                    rho_str = rf"$\rho={rho}$ - Snapping Rhortle"
+                else:
+                    rho_str = rf"$\rho={rho}$"
+                suptitle = rf"[{species} {wf_idx}]   [{wf_fn}]   [{rho_str}]   [$\tau$={tau*1000:.2f}ms]   [HPF at {hpf_cutoff_freq}Hz]   [$\xi_{{\text{{max}}}}={max_xi*1000:.0f}$ms]   [{wf_length}s WF]   [{N_pd_str}]"
+                if dense_stft:
+                    suptitle += f'   [Dense STFT ({seg_spacing*1000}ms)]'
+                
+                
+                if plotting_colossogram:
+                    print("Plotting Colossogram")
+                    plt.close('all')
+                    plt.figure(figsize=(15, 5))
+                    plot_colossogram(coherences, f, xis, tau, max_khz=max_khz, cmap='magma')
+                    for peak_idx in peak_idxs:
+                        plt.scatter(min_xi*1000 + (max_xi*1000)/50, f[peak_idx] / 1000, c='w', marker='>', label="Peak at " + f"{f[peak_idx]:0f}Hz", alpha=0.5)
+                    plt.title(f"Colossogram", fontsize=18)
+                    plt.suptitle(suptitle, fontsize=10)
+                    os.makedirs(f'{fig_folder}\Colossograms', exist_ok=True)
+                    plt.savefig(f'{fig_folder}\Colossograms\{fn_id} (Colossogram).png', dpi=300)
+                    if show_plots:
+                        plt.show()
+                        
+                if plotting_peak_picks:
+                    print("Plotting Peak Picks")
+                    target_xi = 0.01
+                    xi_idx = np.argmin(np.abs(xis - target_xi))
+                    coherence_slice = coherences[:, xi_idx]
+                    psd = get_welch(wf=wf, fs=fs, tauS=tauS)[1]
+                    plt.close('all')
+                    plt.figure(figsize=(11, 8))
+                    plt.suptitle(suptitle)
+                    # Coherence slice plot
+                    plt.subplot(2, 1, 1)
+                    plt.title(rf"Colossogram Slice at $\xi={xis[xi_idx]:.3f}$")
+                    plt.plot(f / 1000, coherence_slice, label=r'$C_{\xi}$, $\xi={target_xi}$')
+                    for peak_idx in peak_idxs:
+                        plt.scatter(f[peak_idx] / 1000, coherence_slice[peak_idx], c='r')
+                    plt.xlabel("Frequency (kHz)")
+                    plt.ylabel(r'$C_{\xi}$')
+                    plt.xlim(0, max_khz)
+                    # PSD plot
+                    plt.subplot(2, 1, 2)
+                    plt.title(rf"Power Spectral Density")
+                    plt.plot(f / 1000, 10*np.log10(psd), label='PSD')
+                    for peak_idx in peak_idxs:
+                        plt.scatter(f[peak_idx] / 1000, 10*np.log10(psd[peak_idx]), c='r')
+                    plt.xlabel("Frequency (kHz)")
+                    plt.ylabel("PSD [dB]")  
+                    plt.legend()
+                    plt.xlim(0, max_khz)
+                    plt.tight_layout()
+                    os.makedirs(f'{fig_folder}\Peak Picks', exist_ok=True)
+                    plt.savefig(f'{fig_folder}\Peak Picks\{fn_id} (Peak Picks).png', dpi=300)
+                    if show_plots:
+                        plt.show()
+                    
+                "FITTING"
+                if plotting_fits:
+                    print(f"Fitting {wf_fn}")
+                    p0 = [1, 1]
+                    bounds = ([0, 0], [np.inf, A_max]) # [T, amp]
+                    fit_func = exp_decay
+                    for peak_idxs, good_peaks in zip([good_peak_idxs, bad_peak_idxs], [True, False]):
+                        # If there are no bad peaks, skip
+                        if len(peak_idxs) == 0:
+                            if good_peaks:
+                                print("No peaks were picked!")
+                            else:
+                                continue
+                        
+                        plt.close('all')
+                        plt.figure(figsize=(12, 10))
+                        plt.suptitle(suptitle)
+
+                        for peak_idx, color, subplot_idx in zip(peak_idxs, colors, [1, 2, 3, 4]):
+                            # Pack all parameters for fit_peak together into a tuple for compactness
+                            peak_fit_params = f, peak_idx, sample_hw, alpha_z, decay_start_max_xi, trim_step, sigma_weighting_power, bounds, p0, coherences, xis, wf_fn, rho
+                            # Fit peak
+                            fit_peak_output = fit_peak(*peak_fit_params)
+                            
+                            # Handle the case where the peak fit failed
+                            if len(fit_peak_output) == 8:
+                                freq, is_signal, is_noise, xi_decayed, decayed_idx, xis_num_cycles, target_coherence, noise_means = fit_peak_output
+                                T, T_std, A, A_std, mse = np.nan, np.nan, np.nan, np.nan, np.nan
+                                # Begin plot
+                                plt.subplot(2, 2, subplot_idx)
+                                plt.title(rf"{freq:.0f}Hz Peak (FIT FAILED)")
+                            # Handle the case where the peak fit succeeded
+                            else:
+                                T, T_std, A, A_std, mse, freq, is_signal, is_noise, decayed_idx, target_coherence, target_coherence_cropped, xis_num_cycles, x_fitted, y_fitted, noise_means = fit_peak_output
+                                # Plot the fit
+                                plt.subplot(2, 2, subplot_idx)
+                                plt.title(rf"{freq:.0f}Hz Peak")
+                                T_label = rf"{T*freq:.0f} \text{{ Cycles}}"
+                                fit_label = rf"$T={T_label}\pm{T_std:.2g}$, $A={A:.2f}\pm{A_std:.2g}$, MSE={mse:.2g}"
+                                plt.plot(x_fitted, y_fitted, color=color, label=fit_label, lw=lw_fit, path_effects=pe_stroke_fit, alpha=alpha_fit, zorder=2)
+                    
+                            # Plot the coherence
+                            plt.scatter(xis_num_cycles[is_signal], target_coherence[is_signal], s=s_signal, edgecolors=edgecolor_signal, marker=marker_signal, color=color, zorder=1, label='Below Noise Floor')
+                            plt.scatter(xis_num_cycles[is_noise], target_coherence[is_noise], s=s_noise, color=color, edgecolors=edgecolor_noise, zorder=1, label='Above Noise Floor')
+                            # Mark decayed point
+                            plt.scatter(xis_num_cycles[decayed_idx], target_coherence[decayed_idx], s=s_decayed, marker=marker_decayed, color=color, edgecolors=edgecolor_decayed, zorder=3)
+                            if plot_noise_means_on_fits:
+                                # plt.scatter(xis_num_cycles, noise_means, label='Noise Mean (Above 12kHz)', s=1, color=colors[4])
+                                plt.plot(xis_num_cycles, noise_means, label='Noise Mean (Above 12kHz)', color=colors[4])
+                            if plot_noise_bin_on_fits:
+                                noise_freq = 15000
+                                noise_target_idx = np.argmin(np.abs(f-noise_freq))
+                                # plt.scatter(xis_num_cycles, coherences[noise_target_idx, :], label=f'Noise Bin ({noise_freq/1000:.0f}kHz)', s=1, color=colors[5])
+                                plt.plot(xis_num_cycles, coherences[noise_target_idx, :], label=f'Noise Bin ({noise_freq/1000:.0f}kHz)', color=colors[5])
+                            
+                            # Finish plot
+                            plt.xlabel(r'# Cycles')
+                            plt.ylabel(r'$C_{\xi}$')           
+                            plt.ylim(0, 1)
+                            plt.legend()
+                            
+                            # Add params to a row dict
+                            if good_peaks:
+                                rows.append({
+                                    'Species':species,
+                                    'WF Index':wf_idx,
+                                    'Filename':wf_fn,
+                                    'Frequency':freq,
+                                    'T':T,
+                                    'T_std':T_std,
+                                    'A':A,
+                                    'A_std':A_std,
+                                    'MSE':mse,
+                                })
+                            
+                        # Book it!
+                        plt.tight_layout()
+                        os.makedirs(fig_folder, exist_ok=True)
+                        fits_str = 'Fits' if good_peaks else 'Bad Fits'   
+                        os.makedirs(f'{fig_folder}\{fits_str}', exist_ok=True) 
+                        plt.savefig(f'{fig_folder}\{fits_str}\{fn_id} ({fits_str}).png', dpi=300)
+                        if show_plots:
+                            plt.show()
+                        
+                    
+                    
+                    
+                    
+                        
+    # Save parameter data as xlsx
+    df = pd.DataFrame(rows)
+    N_xi_fitted_parameters_fn = rf'{fig_folder}\N_xi Fitted Parameters (rho={rho})'
+    df.to_excel(rf'{N_xi_fitted_parameters_fn}.xlsx', index=False)
+                        
+                        
+            
--- N_xi Fits/N_xi_fit_funcs.py
+++ N_xi Fits/N_xi_fit_funcs.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.io as sio
 import scipy as sp
 from scipy.optimize import curve_fit
+from scipy.signal import find_peaks
 
 def get_wf(wf_fn=None, species=None, wf_idx=None):
     if wf_fn is None:
@@ -23,44 +24,44 @@ def get_wf(wf_fn=None, species=None, wf_idx=None):
     match wf_fn:
         # Anoles
         case 'AC6rearSOAEwfB1.mat': #0
-            peak_freqs = [1233, 2164, 3714, 4500]
-            bad_fit_freqs = []
+            good_peak_freqs = [1233, 2164, 3714, 4500]
+            bad_peak_freqs = []
         case 'ACsb4rearSOAEwf1.mat': #1
-            peak_freqs = [964, 3031, 3160, 3957]
-            bad_fit_freqs = []
+            good_peak_freqs = [964, 3031, 3160, 3957]
+            bad_peak_freqs = []
         case 'ACsb24rearSOAEwfA1.mat': #2    
-            peak_freqs = [1809, 2169, 3112, 3478]
-            bad_fit_freqs = []
+            good_peak_freqs = [1809, 2169, 3112, 3478]
+            bad_peak_freqs = []
         case 'ACsb30learSOAEwfA2.mat': #3
-            peak_freqs = [1803, 2137, 2406, 2778]
-            bad_fit_freqs = []
+            good_peak_freqs = [1803, 2137, 2406, 2778]
+            bad_peak_freqs = []
         # Humans
         case 'ALrearSOAEwf1.mat': #0
-            peak_freqs = [2665, 2945, 3219, 3865]
-            bad_fit_freqs = []
+            good_peak_freqs = [2665, 2945, 3219, 3865]
+            bad_peak_freqs = []
         case 'JIrearSOAEwf2.mat': #1
-            peak_freqs = [2342, 3402, 8312, 8678]
-            bad_fit_freqs = []
+            good_peak_freqs = [2342, 3402, 8312, 8678]
+            bad_peak_freqs = []
         case 'LSrearSOAEwf1.mat': #2
-            peak_freqs = [732, 985, 1637, 2229]
-            bad_fit_freqs = []
+            good_peak_freqs = [732, 985, 1637, 2229]
+            bad_peak_freqs = []
         case 'TH13RearwaveformSOAE.mat': #3
-            peak_freqs = [904, 1518, 2040, 2697]
-            bad_fit_freqs = []
+            good_peak_freqs = [904, 1518, 2040, 2697]
+            bad_peak_freqs = []
         # Owls
         case 'Owl2R1.mat': #0
-            peak_freqs = [4355, 7451, 8458, 9039]
-            bad_fit_freqs = []
+            good_peak_freqs = [4355, 7451, 8458, 9039]
+            bad_peak_freqs = []
         case 'Owl7L1.mat': #1
-            peak_freqs = [6896, 7941, 8861, 9271]
-            bad_fit_freqs = []
+            good_peak_freqs = [6896, 7941, 8861, 9271]
+            bad_peak_freqs = []
         case 'TAG6rearSOAEwf1.mat': #2
-            peak_freqs = [5626, 8096, 8484, 9862]
-            bad_fit_freqs = []
+            good_peak_freqs = [5626, 8096, 8484, 9862]
+            bad_peak_freqs = []
         case 'TAG9rearSOAEwf2.mat': #3
-            peak_freqs = [4931, 6993, 7450, 9878]
-            bad_fit_freqs = []
-    return wf, wf_fn, fs, np.array(peak_freqs), np.array(bad_fit_freqs)
+            good_peak_freqs = [4931, 6993, 7450, 9878]
+            bad_peak_freqs = []
+    return wf, wf_fn, fs, np.array(good_peak_freqs), np.array(bad_peak_freqs)
         
 def get_fn(species, idx):
     match species:
@@ -115,7 +116,11 @@ def get_is_signal(coherences, f, xis, f_target_idx, f_noise=12000, sample_hw=10,
         # Calculate z test statistic
         sample_mean = np.mean(coherence_sample)
         z = (sample_mean - noise_means[xi_idx]) / (noise_stds[xi_idx]/np.sqrt(len(coherence_sample)))
-        # Calculate p-value for a one-tailed test (sf = survival function = 1 - cdf)
+        # As n (sample size) gets smaller, sqrt(n) gets smaller → the standard error increases -> more likely to fail to reject null -> more noise bins.
+        # Think of the standard error as the "blur" or "noise" around your estimate. 
+        # Smaller samples = blurrier estimates = harder to be confident your sample mean is really different from the null mean.
+        # Calculate p-value for a one-tailed test (sf = survival function = 1 - cdf for a right tailed z test)
+        # p = Pr(Z > z), i.e., sample mean > null mean
         p = sp.stats.norm.sf(z)
         is_signal[xi_idx] = p < z_alpha
     target_coherences=coherences[f_target_idx, :]
@@ -132,24 +137,40 @@ def get_is_signal(coherences, f, xis, f_target_idx, f_noise=12000, sample_hw=10,
         print(f"Signal at {f[f_target_idx]:.0f}Hz never decays!")
     xi_decayed = xis[decayed_idx]
     
-    return is_signal, target_coherences, xi_decayed, decayed_idx
+    return is_signal, target_coherences, xi_decayed, decayed_idx, noise_means
 
 def exp_decay(x, T, amp):
     return amp * np.exp(-x/T)
 
-def fit_peak(f, peak_idx, sample_hw, z_alpha, min_fit_xi_idx, trim_step, sigma_weighting_power, bounds, p0, coherences, xis, wf_fn, rho):
+def fit_peak(f, peak_idx, sample_hw, z_alpha, decay_start_max_xi, trim_step, sigma_weighting_power, bounds, p0, coherences, xis, wf_fn, rho):
+    # Get freq
+    freq = f[peak_idx]
+    # Convert the xis array to number of cycles
+    xis_num_cycles = xis  * freq
+    
     if sigma_weighting_power == 0:
         get_fit_sigma = lambda y, sigma_weighting_power: np.ones(len(y))
     else: 
-        get_fit_sigma = lambda y, sigma_weighting_power: 1 / (y**sigma_weighting_power+ 1e-9)
-    freq = f[peak_idx]  
+        get_fit_sigma = lambda y, sigma_weighting_power: 1 / (y**sigma_weighting_power+ 1e-9)  
     # Calculate signal vs noise and point of decay
-    is_signal, target_coherence, xi_decayed, decayed_idx = get_is_signal(coherences, f, xis, peak_idx, f_noise=12000, sample_hw=sample_hw, z_alpha=z_alpha)
+    is_signal, target_coherence, xi_decayed, decayed_idx, noise_means = get_is_signal(coherences, f, xis, peak_idx, f_noise=12000, sample_hw=sample_hw, z_alpha=z_alpha)
     is_noise = ~is_signal
     
     # Curve Fit
-    print(f"Fitting exp decay to {freq}Hz peak on {wf_fn} with rho={rho}")
-    fit_start_idx = np.argmax(target_coherence[min_fit_xi_idx:]) + min_fit_xi_idx
+    print(f"Fitting exp decay to {freq:.0f}Hz peak on {wf_fn} with rho={rho}")
+    # Find where to start the fit as the latest peak in the range defined by xi=[0, decay_start_max_xi] 
+    decay_start_max_xi_idx = np.argmin(np.abs(xis-decay_start_max_xi))
+    maxima = find_peaks(target_coherence[:decay_start_max_xi_idx])[0]
+    num_maxima = len(maxima)
+    match num_maxima:
+        case 1:
+            fit_start_idx = maxima[0]
+        case 2:
+            fit_start_idx = maxima[1]
+        case 0:
+            fit_start_idx = 0
+        case _:
+            raise RuntimeError(f"Three or more peaks found in first {decay_start_max_xi*10:.0f}ms of xi!")
     x_to_fit = xis[fit_start_idx:decayed_idx]
     y_to_fit = target_coherence[fit_start_idx:decayed_idx]
     sigma = get_fit_sigma(y_to_fit, sigma_weighting_power) 
@@ -168,28 +189,29 @@ def fit_peak(f, peak_idx, sample_hw, z_alpha, min_fit_xi_idx, trim_step, sigma_w
             sigma = sigma[trim_step:-trim_step]
             
             print(f"Fit failed (attempt {failures}): — trimmed to {len(x_to_fit)} points")
+            
     if popt is None:
-        raise RuntimeError(f"Curve fit failed after all attempts ({freq:.0f}Hz from {wf_fn})")
+        print("Curve fit failed after all attempts ({freq:.0f}Hz from {wf_fn})")
+        # raise RuntimeError(f"Curve fit failed after all attempts ({freq:.0f}Hz from {wf_fn})")
+        return freq, is_signal, is_noise, xi_decayed, decayed_idx, xis_num_cycles, target_coherence, noise_means
     else:
         print("Fitting successful!")
-    # Get the paramters and stndard devition
-    perr = np.sqrt(np.diag(pcov))
-    tc = popt[0]
-    tc_std = perr[0]
-    A = popt[1]
-    A_std = perr[1]
-    # Convert the xis array to number of cycles
-    xis_num_cycles = xis  * freq
-    # Get the fitted exponential decay
-    y_fitted = exp_decay(x_to_fit, *popt)
-    # Convert the x array for the fit into number of cycles
-    x_fitted = x_to_fit * freq
-    # Get coherence array that corresponds to the final fit
-    target_coherence_cropped = target_coherence[fit_start_idx:decayed_idx]
-    if failures > 0:
-        target_coherence_cropped = target_coherence[failures*trim_step:-failures*trim_step]
+        # Get the paramters and stndard devition
+        perr = np.sqrt(np.diag(pcov))
+        T = popt[0]
+        T_std = perr[0]
+        A = popt[1]
+        A_std = perr[1]
+        # Get the fitted exponential decay
+        y_fitted = exp_decay(x_to_fit, *popt)
+        # Convert the x array for the fit into number of cycles
+        x_fitted = x_to_fit * freq
+        # Get coherence array that corresponds to the final fit
+        target_coherence_cropped = target_coherence[fit_start_idx:decayed_idx]
+        if failures > 0:
+            target_coherence_cropped = target_coherence[failures*trim_step:-failures*trim_step]
 
-    # Calculate MSE
-    mse = np.mean((y_fitted - target_coherence_cropped)**2)
+        # Calculate MSE
+        mse = np.mean((y_fitted - target_coherence_cropped)**2)
     
-    return tc, tc_std, A, A_std, mse, freq, is_signal, is_noise, decayed_idx, target_coherence, target_coherence_cropped, xis_num_cycles, x_fitted, y_fitted
+        return T, T_std, A, A_std, mse, freq, is_signal, is_noise, decayed_idx, target_coherence, target_coherence_cropped, xis_num_cycles, x_fitted, y_fitted, noise_means
