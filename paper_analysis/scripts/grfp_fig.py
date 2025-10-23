eeda19611694d9d2eef8b820d7cb329f7d640a9e
seththepeacock <seththepeacock@gmail.com>
2025-10-23 17:03:18 -0400

    a few plot updates

--- paper_analysis/scripts/grfp_fig.py
+++ paper_analysis/scripts/grfp_fig.py
@@ -11,11 +11,11 @@ tau_pkl_folder = os.path.join('paper_analysis', 'pickles')
 os.makedirs(paper_figures_folder, exist_ok=True)
 
 
-"Static vs Dynamic Colossogram (TIGHT)"
+"Static vs Dynamic Colossogram (GRFP Proposal)"
 
 # Choose subject
-species = "Anole"
-wf_idx = 0
+species = "Tokay"
+wf_idx = 2
 
 # Get waveform
 wf, wf_fn, fs, good_peak_freqs, bad_peak_freqs = get_wf(
@@ -23,6 +23,20 @@ wf, wf_fn, fs, good_peak_freqs, bad_peak_freqs = get_wf(
     wf_idx=wf_idx,
 )
 
+# Plot params
+xlabel = "Timescale [ms]"
+ylabel = "Frequency [kHz]"
+cmap = "magma"
+cbar_label = "Phase Self-Coherence"
+fontsize = 8
+fontsize_ticks = 5
+labelpad = 5
+ymax = 3.0
+ymin = 1.25
+xmin = 5
+xmax = 30
+vmax_1 = 1.0
+vmax_2 = 0.3
 
 # Get/set coherence params
 filter_meth=None
@@ -38,12 +52,15 @@ xi_min_s = 0.0005
 xi_max_s = 0.1
 nfft = 2**14
 const_N_pd = 0
+show_plots = 1
+
+
 
 
 tau = get_precalc_tau_from_bw(bw, fs, win_type, tau_pkl_folder)
 
 # Start building LCC kwargs dict with constants
-lcc_kwargs_tight = {
+lcc_kwargs_alt = {
     "wf_len_s": wf_len_s,
     "filter_meth": filter_meth,
     "pw": pw,
@@ -66,88 +83,50 @@ lcc_kwargs_tight = {
 
 
 # INITIALIZE PLOT
-plt.figure(figsize=(8.5 - 2, 4))
+plt.figure(figsize=(8.5 - 2, 2))
 
-# Set labels
-xlabel = "Timescale [ms]"
-ylabel = "Frequency [kHz]"
-cmap = "magma"
-cbar_label = "Coherence"
-fontsize_tight = 10
-labelpad_tight = 5
-ymax = 4
-ymin = 1
-xmax = 50
+
 
 
 # Load Colossogram
-cgram_dict = load_calc_colossogram(**lcc_kwargs_tight)
+cgram_dict = load_calc_colossogram(**lcc_kwargs_alt)
 
 # Load everything that wasn't explicitly "saved" in the filename
 colossogram = cgram_dict["colossogram"]
 xis_s = cgram_dict["xis_s"]
 f = cgram_dict["f"]
-
-xmin = xis_s[0] * 10000
 
 # MAKE PLOT
 plt.subplot(1, 3, 3)
-cbar = pc.plot_colossogram(xis_s, f, colossogram, cmap=cmap, return_cbar=True)
+cbar = pc.plot_colossogram(xis_s, f, colossogram, cmap=cmap, return_cbar=True, vmax=vmax_2)
+plt.title("Dynamic", fontsize=fontsize)
 # Set Chris' requested fontsizes
 ax = plt.gca()
 
-
-# ax.set_xlabel(xlabel, labelpad=labelpad_tight, fontsize=fontsize_tight)
 ax.set_ylabel(None)
+ax.set_xlabel(None)
 
-ax.tick_params("both", labelsize=fontsize_tight)
-cbar.ax.set_ylabel(cbar_label, labelpad=labelpad_tight)
-cbar.ax.yaxis.label.set_fontsize(fontsize_tight)
-cbar.ax.tick_params(labelsize=fontsize_tight)
+ax.tick_params("both", labelsize=fontsize_ticks)
+cbar.ax.set_ylabel(cbar_label, labelpad=labelpad)
+cbar.ax.yaxis.label.set_fontsize(fontsize)
+cbar.ax.tick_params(labelsize=fontsize_ticks)
 
 
-plt.ylim(1, 3.5)
+
+plt.ylim(ymin, ymax)
 plt.xlim(xmin, xmax)
-
-
-"Repeat for static"
-lcc_kwargs_tight["win_meth"] = {"method": "static", "win_type": win_type}
-bw_static = 150
-tau_static_150 = get_precalc_tau_from_bw(bw_static, fs, win_type, tau_pkl_folder)
-lcc_kwargs_tight['tau'] = tau_static_150
-
-# Load Colossogram
-cgram_dict = load_calc_colossogram(**(lcc_kwargs_tight))
-
-# Load everything that wasn't explicitly "saved" in the filename
-colossogram = cgram_dict["colossogram"]
-xis_s = cgram_dict["xis_s"]
-f = cgram_dict["f"]
-
-# MAKE PLOT
-plt.subplot(1, 3, 2)
-cbar = pc.plot_colossogram(xis_s, f, colossogram, cmap=cmap, return_cbar=True)
-# Set Chris' requested fontsizes
-ax = plt.gca()
-
-
-ax.set_xlabel(xlabel, labelpad=labelpad_tight, fontsize=fontsize_tight)
-ax.set_ylabel(None)
-
-ax.tick_params("both", labelsize=fontsize_tight)
-cbar.remove()
-
-plt.ylim(1, 3.5)
-plt.xlim(xmin, xmax)
-
 
 "Repeat for static (50Hz)"
+# Switch to static
+lcc_kwargs_alt["win_meth"] = {"method": "static", "win_type": win_type}
+
+# Change bandwidth
 bw_static = 50
 tau_static_50 = get_precalc_tau_from_bw(bw_static, fs, win_type, tau_pkl_folder)
-lcc_kwargs_tight["tau"] = tau_static_50
+lcc_kwargs_alt["tau"] = tau_static_50
 
 # Load Colossogram
-cgram_dict = load_calc_colossogram(**(lcc_kwargs_tight))
+cgram_dict = load_calc_colossogram(**(lcc_kwargs_alt))
 
 # Load everything that wasn't explicitly "saved" in the filename
 colossogram = cgram_dict["colossogram"]
@@ -156,22 +135,65 @@ f = cgram_dict["f"]
 
 # MAKE PLOT
 plt.subplot(1, 3, 1)
-cbar = pc.plot_colossogram(xis_s, f, colossogram, cmap=cmap, return_cbar=True)
+plt.title("50Hz Bandwidth", fontsize=fontsize)
+cbar = pc.plot_colossogram(xis_s, f, colossogram, cmap=cmap, return_cbar=True, vmax=vmax_1)
 # Set Chris' requested fontsizes
 ax = plt.gca()
 
 
 # ax.set_xlabel(xlabel, labelpad=labelpad_tight, fontsize=fontsize_tight)
-ax.set_ylabel(ylabel, labelpad=labelpad_tight, fontsize=fontsize_tight)
+ax.set_ylabel(ylabel, labelpad=labelpad, fontsize=fontsize)
+ax.set_xlabel(None)
 
-ax.tick_params("both", labelsize=fontsize_tight)
+ax.tick_params("both", labelsize=fontsize_ticks)
 
-cbar.remove()
 
-plt.ylim(1, 3.5)
+# cbar.remove()
+cbar.ax.set_ylabel(None)
+cbar.ax.tick_params(labelsize=fontsize_ticks)
+
+plt.ylim(ymin, ymax)
+plt.xlim(xmin, xmax)
+
+
+"Repeat for static (150Hz)"
+bw_static = 150
+tau_static_150 = get_precalc_tau_from_bw(bw_static, fs, win_type, tau_pkl_folder)
+lcc_kwargs_alt['tau'] = tau_static_150
+
+# Load Colossogram
+cgram_dict = load_calc_colossogram(**(lcc_kwargs_alt))
+
+# Load everything that wasn't explicitly "saved" in the filename
+colossogram = cgram_dict["colossogram"]
+xis_s = cgram_dict["xis_s"]
+f = cgram_dict["f"]
+
+
+# MAKE PLOT
+plt.subplot(1, 3, 2)
+plt.title("150Hz Bandwidth", fontsize=fontsize)
+cbar = pc.plot_colossogram(xis_s, f, colossogram, cmap=cmap, return_cbar=True, vmax=vmax_2)
+# Set Chris' requested fontsizes
+ax = plt.gca()
+
+
+
+ax.set_xlabel(xlabel, labelpad=labelpad, fontsize=fontsize)
+ax.set_ylabel(None)
+
+ax.tick_params("both", labelsize=fontsize_ticks)
+# cbar.remove()
+cbar.ax.set_ylabel(None)
+cbar.ax.tick_params(labelsize=fontsize_ticks)
+
+plt.ylim(ymin, ymax)
 plt.xlim(xmin, xmax)
 
 
 plt.tight_layout()
 
 plt.savefig(os.path.join(paper_figures_folder, f"dyn win cgram fig.jpg"), dpi=500)
+
+if show_plots:
+    plt.show()
